6ec0fe90aafda4c398e47eee01c51f2307edd85a
parsaaes <parsaaes@gmail.com>
2019-04-18 19:44:28 +0430

    Add some examples

--- fcm.py
+++ fcm.py
@@ -19,6 +19,18 @@ def init_color_dict(number_of_clusters):
         color_dict[i] = np.random.rand(1, 3)
 
 
+# for testing only - generate n normalized around 3 centers
+def generate_2d_dataset_with_3_centers(n):
+    res = []
+    sigma = [[0.1, 0.25], [0.5, 0.1], [0.2, 0.4]]
+    mu = [[0.5, 1.5], [2, 0.5], [3.5, 2.5]]
+    for i in range(n):
+        x = np.random.standard_normal() * sigma[i % 3][0] + mu[i % 3][0]
+        y = np.random.standard_normal() * sigma[i % 3][1] + mu[i % 3][1]
+        res.append([x, y])
+    return res
+
+
 # generate n data each with d features
 def generate_dataset(n, d, data_min, data_max):
     res = []
@@ -32,6 +44,7 @@ def generate_dataset(n, d, data_min, data_max):
 
 def print_dataset(dataset):
     print(np.asarray(dataset))
+
 
 def plot_2d_dataset(dataset, markers=None, labels=None):
     # x, y = np.asarray(dataset).T
@@ -47,12 +60,12 @@ def plot_2d_dataset(dataset, markers=None, labels=None):
     for marker in markers:
         plt.scatter(marker[0], marker[1], s=100, c=color_dict[markers.index(marker)], marker='+')
     plt.show()
-    #plt.savefig(str(time.time()) + '.png')
-    #plt.close()
+    # plt.savefig(str(time.time()) + '.png')
+    # plt.close()
 
 
-def plot_with_markers(clusters, dataset, membership):
-    labels = get_cluster_label(membership)
+def plot_2d_with_markers(clusters, dataset, membership):
+    labels = get_cluster_labels(membership)
     plot_2d_dataset(dataset, markers=clusters, labels=labels)
 
 
@@ -114,7 +127,7 @@ def update_membership_matrix(dataset, clusters, m):
     return membership_matrix
 
 
-def get_cluster_label(membership_matrix):
+def get_cluster_labels(membership_matrix):
     res = []
     for membership in membership_matrix:
         max_index = membership.index(max(membership))
@@ -130,22 +143,26 @@ def fcm(cluster_no, iterations, dataset, m=2):
     for i in range(iterations):
         clusters = update_cluster_centers(dataset, membership, m)
         membership = update_membership_matrix(dataset, clusters, m)
-        plot_with_markers(clusters, dataset, membership)
+        # plot_2d_with_markers(clusters, dataset, membership) # this is only working if dataset is 2d
     return clusters, membership
 
 
 def run_fcm_on_a_2d_dataset():
     features = 2
     number_of_data = 100
-    number_of_clusters = 2
-    iterations = 5
+    number_of_clusters = 3
+    iterations = 10
     data_max = 3
     data_min = 0
-    dataset = generate_dataset(number_of_data, features, data_min, data_max)
-    init_color_dict(number_of_clusters)
-    cc, mm = fcm(number_of_clusters, iterations, dataset)
+    dataset = generate_dataset(number_of_data, features, data_min, data_max)  # generate a random dataset
+    # dataset = generate_2d_dataset_with_3_centers(number_of_data)
+    init_color_dict(number_of_clusters)  # init colors for plotting
+    cluster_centers, final_memberships = fcm(number_of_clusters, iterations, dataset)  # run FCM
+    plot_2d_with_markers(cluster_centers, dataset, final_memberships)  # plot final result
+    final_labels = get_cluster_labels(final_memberships)  # get labels
     print("cluster centers:")
-    print_dataset(cc)
+    print_dataset(cluster_centers)
+
 
 if __name__ == '__main__':
     run_fcm_on_a_2d_dataset()
